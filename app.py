f0635ec5f2019f2572dfa5ea0a5824f7b5c09ecd
Akib Sadmanee <sadmanee@hawaii.edu>
2023-05-22 15:45:39 -1000

    reduced api keys

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 cors = CORS(app)  
 
 receipts = [
-    {'id': 1, 'url': 'https://raw.githubusercontent.com/AkibSadmanee/receipts-generator/main/images/1.jpg', 'title': 'Image 1', 'label': '10/25/21', 'content': 'Walmart'},
-    {'id': 2, 'url': 'https://raw.githubusercontent.com/AkibSadmanee/receipts-generator/main/images/2.jpg', 'title': 'Image 2', 'label': '10/26/21', 'content': 'Walmart'},
-    {'id': 3, 'url': 'https://raw.githubusercontent.com/AkibSadmanee/receipts-generator/main/images/3.jpg', 'title': 'Image 3', 'label': '10/27/21', 'content': 'Walmart'},
-    {'id': 4, 'url': 'https://raw.githubusercontent.com/AkibSadmanee/receipts-generator/main/images/4.jpg', 'title': 'Image 4', 'label': '10/28/21', 'content': 'Walmart'},
-    {'id': 5, 'url': 'https://raw.githubusercontent.com/AkibSadmanee/receipts-generator/main/images/5.jpg', 'title': 'Image 5', 'label': '10/29/21', 'content': 'Walmart'},
-    {'id': 6, 'url': 'https://raw.githubusercontent.com/AkibSadmanee/receipts-generator/main/images/6.jpg', 'title': 'Image 6', 'label': '10/30/21', 'content': 'Walmart'},
-    {'id': 7, 'url': 'https://raw.githubusercontent.com/AkibSadmanee/receipts-generator/main/images/7.jpg', 'title': 'Image 7', 'label': '10/31/21', 'content': 'Walmart'}
+    {'id': 1, 'url': 'https://raw.githubusercontent.com/AkibSadmanee/receipts-generator/main/images/1.jpg'},
+    {'id': 2, 'url': 'https://raw.githubusercontent.com/AkibSadmanee/receipts-generator/main/images/2.jpg'},
+    {'id': 3, 'url': 'https://raw.githubusercontent.com/AkibSadmanee/receipts-generator/main/images/3.jpg'},
+    {'id': 4, 'url': 'https://raw.githubusercontent.com/AkibSadmanee/receipts-generator/main/images/4.jpg'},
+    {'id': 5, 'url': 'https://raw.githubusercontent.com/AkibSadmanee/receipts-generator/main/images/5.jpg'},
+    {'id': 6, 'url': 'https://raw.githubusercontent.com/AkibSadmanee/receipts-generator/main/images/6.jpg'},
+    {'id': 7, 'url': 'https://raw.githubusercontent.com/AkibSadmanee/receipts-generator/main/images/7.jpg'}
 ]
 
 # Route to get all receipts
