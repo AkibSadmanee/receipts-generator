3b71e9aa876fcea3aee642e6d0e0944eb1cf19ae
Akib Sadmanee <sadmanee@hawaii.edu>
2023-06-04 15:47:48 -1000

    changed receipts

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ cors = CORS(app)
 
 receipts = [
     {'id': 1, 'url': 'https://raw.githubusercontent.com/AkibSadmanee/receipts-generator/main/images/1.jpg', 'store': 'Target', 'total': '0.41', 'status': False, 'date': '05/12/2018', 'logo': f'https://raw.githubusercontent.com/AkibSadmanee/receipts-generator/main/logo/target.png'},
-    {'id': 2, 'url': 'https://raw.githubusercontent.com/AkibSadmanee/receipts-generator/main/images/2.jpg', 'store': 'Walmart', 'total': '20.67', 'status': False, 'date': '--/--/----', 'logo': f'https://raw.githubusercontent.com/AkibSadmanee/receipts-generator/main/logo/walmart.png'},
+    {'id': 2, 'url': 'https://raw.githubusercontent.com/AkibSadmanee/receipts-generator/main/images/2.jpg', 'store': 'Apple', 'total': '1931.13', 'status': False, 'date': '01/17/2009', 'logo': f'https://raw.githubusercontent.com/AkibSadmanee/receipts-generator/main/logo/walmart.png'},
     {'id': 3, 'url': 'https://raw.githubusercontent.com/AkibSadmanee/receipts-generator/main/images/3.jpg', 'store': 'Safeway', 'total': '28.52', 'status': False, 'date': '08/04/2022', 'logo': f'https://raw.githubusercontent.com/AkibSadmanee/receipts-generator/main/logo/safeway.png'},
     {'id': 4, 'url': 'https://raw.githubusercontent.com/AkibSadmanee/receipts-generator/main/images/4.jpg', 'store': '------', 'total': '-.--', 'status': True, 'date': '--/--/----', 'logo': 'https://s3.amazonaws.com/appgyver.assets/composer3/images/image_placeholder.png'},
 ]
